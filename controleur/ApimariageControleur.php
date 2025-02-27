0d4c8a47c53ff63d389b270c0faf6e30854c47f3
Paul 2617 <paulpassanikone@gmail.com>
2025-02-27 15:06:18 +0000

    Aujourduit

--- controleur/ApimariageControleur.php
+++ controleur/ApimariageControleur.php
@@ -1,4 +1,26 @@
 <?php
+if (isset($_POST["envoyer"]) and !empty($_POST["envoyer"])){
+    if (isset($_POST["jour"]) and !empty($_POST["jour"])){
+        $jour = htmlspecialchars($_POST["jour"]);
+        if (isset($_POST["mois"]) and !empty($_POST["mois"])){
+            $mois = htmlspecialchars($_POST["mois"]);
+            if (isset($_POST["numero"]) and !empty($_POST["numero"])){
+                $numero = htmlspecialchars($_POST["numero"]);
+                $timestamp = time();
+                $anne = date('Y', $timestamp);
+               $date_m = "$jour-$mois-$anne";
+              echo   $dateMariage = strtotime($date_m); 
+            }else{
+            $erreur = 'Veuillez le Numéro';
+        }
+        }else{
+            $erreur = 'Veuillez le mois';
+        }
+    }else{
+        $erreur = 'Veuillez le jours';
+    }
+}
+
 
 require_once ($views);
 
--- views/ApimariagePage.php
+++ views/ApimariagePage.php
@@ -1,32 +1,49 @@
 <div class='blocAcceul'>
     <section class='BlocBac'>
         <form method="POST" enctype="multipart/form-data">
-            <h1>Ajouter api</h1>
+            <h1 class='eooez'>Ajouter api mariage</h1>
 
             <h5>Jour</h5>
-            <input class="form_input" type="text" placeholder="Jour" name="jour"
-                value="<?php if (isset($nom_marie)) {echo $nom_marie;} ?>">
+            <input class="form_input" type="number" placeholder="Jour" name="jour" 
+            min="1" max="31" required
+                value="<?php if (isset($jour)) {echo $jour;} ?>">
             <h5>Mois</h5>
-            <input class="form_input" type="text" placeholder="Mois" name="mois"
-                value="<?php if (isset($nom_mariee)) {echo $nom_mariee;} ?>">
+
+                <select class="form_input" name="mois">
+                    <option value="<?php if (isset($mois)) {echo $mois;} ?>">Sélectionne Mois</option>
+                    <option value="1">Janvier</option>
+                    <option value="2">Février</option>
+                    <option value="3">Mars</option>
+                    <option value="4">Avril</option>
+                    <option value="5">Mai</option>
+                    <option value="6">Juin</option>
+                    <option value="7">Juillet</option>
+                    <option value="8">Août</option>
+                    <option value="9">Septembre</option>
+                    <option value="10">Octobre</option>
+                    <option value="11">Novembre</option>
+                    <option value="12">Décembre</option>
+                </select>
+                <h5>Numéro</h5>
+            <input class="form_input" type="number" placeholder="Numéro" name="numero"
+                value="<?php if (isset($numero)) {echo $numero;} ?>">
             <div class="bloc_taille">
                 <div class="form-element">
-                    <input type="checkbox" name="options[]" value="Snap" id="Snap">
+                    <input type="checkbox" name="Snap" value="Snap" id="Snap">
                     <label for="Snap">
                         <div class="title">Filtre Snap</div>
                     </label>
                 </div>
 
                 <div class="form-element">
-                    <input type="checkbox" name="options[]" value="Menu" id="Menu">
-                    <label for="Menu">
-                        <div class="title">Menu</div>
+                    <input type="checkbox" name="SMS" value="SMS" id="SMS">
+                   <label for="SMS">
+                        <div class="title">SMS invite</div>
                     </label>
-                </div>
+                </div> 
             </div>
             <input class="boutton_inpute" class="submit" type="submit" value="Envoyer" name="envoyer">
             <?php if (isset($erreur)) { ?> <h2 class="erreur"><?php echo $erreur ?></h1> <?php } ?>
-                <h1 class='text_d'>Suivre mes<a class='link' href=""> invitations</a></h1>
 
         </form>
     </section>
